28f52dd23f04cb126500217ebd2fca77889f0b45
Ethan Holland <ethanjholland@gmail.com>
2019-01-05 14:39:41 -0500

    refactored code to allow methods to be used for other purposes

--- haiku.py
+++ haiku.py
@@ -17,87 +17,38 @@ wordDict = cmudict.dict()
 filepath = 'C:\\' #root folder - making this more specific will speed up file finding
 fonts=["C:\\windows\\fonts\\{}.ttf".format(name) for name in ['couri','bookosi','belli']] #paths to font files
 
-def countSyllables(word):
-    word=re.sub(r"[^a-z]",'',word.lower())
-    if word in wordDict:
-        return len(list(y for y in wordDict[word][0] if y[-1] in '0123456789'))
-    return -1
+def getFiles(root='C:\\', verbose=False):
+    if verbose:
+        print('looking for files (this may take a while)')
 
-def getFiles(root=sys.path[0]):
     wordDocs=[]
     for path, subdirs, files in os.walk(root):
         for name in files:
             if name.endswith('.doc') or name.endswith('.docx'):
                 wordDocs.append(os.path.join(path, name))
+
+    if verbose:
+        print("found {} word documents".format(len(wordDocs)))
     return wordDocs
 
-def getWords(sentence):
-    sentence=sentence.strip().replace('%',' percent').replace("'s "," ")
-    sentence=re.sub(r"(^|\s)-(\d+\.?\d*)",r" negative \2",sentence).strip() #deal with negative numbers (since we split on - later)
-    words=[word for word in re.split(r"\s+|-|–",sentence) if word]
-    i=0
-    while i<len(words):
-        word=words[i]
-        if word!='infinity' and word!='nan': #ignore cases that break num2words
-            try:
-                #if word is a number, convert to the spoken form so we can count syllables
-                conv=num2words(word).replace('point zero','').strip()
-                words.remove(word)
-                for newWord in re.split(r"\s+|-|–",conv):
-                    words.insert(i,newWord)
-                    i+=1
-            except TypeError:
-                pass #not a number
-        i+=1
-    return words
-
-def displayHaiku(text,writefile):
-    #get picsum image
-    response = requests.get('https://picsum.photos/740/525/?random') #dimensions of a postcard
-    img = Image.open(BytesIO(response.content))
-
-    #generate properties
-    font = ImageFont.truetype(random.choice(fonts), randint(30,40)) #choose typeface, font size from distributions
-    position = (randint(10,70),randint(100,300)) #choose x,y coordinates
-    color = getColor(img, position)
-
-    #write text and save
-    draw = ImageDraw.Draw(img)
-    draw.text(position,text,color,font=font)
-    img.save(writefile)  
-
-def getColor(img, position):
-    #get avg lightness of pixels in the approximate location of the text
-    approxWidth=500
-    approxHeight=150
-    tot=0
-    for i in range(position[0], position[0]+approxWidth):
-        for j in range(position[1],position[1]+approxHeight):
-            tot+=sum(img.getpixel((i,j))) #add r+g+b
-    avg=tot/approxWidth/approxHeight/3
-
-    #take gray as far as possible from avg and flatten to black or white
-    if (avg+122)%255>122:
-        return (255,255,255)
-    else:
-        return (0,0,0) 
-
-def main(verbose=False):
-    os.makedirs('images', exist_ok=True) #make images folder if doesn't already exist
-    linecount=[5,12,17] #syllables after each line of a haiku
-    found=0
-    
+def extractTexts(files, verbose=False):
+    #get texts from files
     if verbose:
-        print('looking for files (this may take a while)')
-    files=getFiles(filepath)
-    if verbose:
-        print("found {} word documents".format(len(files)))
+        print('extracting text from files')
+
+    texts = []
     for filename in files:
         try:
-            text=docx2txt.process(filename)
+            texts.append(docx2txt.process(filename))
         except:
-            continue #corrupted file
+            pass #corrupted file
+    return texts
 
+def extractHaiku(texts, verbose=False):
+    haikus = []
+    linecount=[5,12,17] #syllables after each line of a haiku
+
+    for text in texts:
         for sentence in re.split(r"\.|\n+",text):
             display='' #sentence formatted like a haiku
             linecheck=[False,False,False]
@@ -122,10 +73,76 @@ def main(verbose=False):
                     break
 
             if sum==17 and all(linecheck): #found a haiku
-                found+=1
+                haikus.append(display.strip())
                 if verbose:
-                    print("haiku {} ({})\n{}\n-".format(found,filename,display.strip()))
-                displayHaiku(display.strip(),'images/haiku-{}.png'.format(found))
+                    print(display.strip(),'___',sep='\n')
+    return haikus
+
+def displayHaiku(haikus, directory='images', verbose=False):
+    for index, text in enumerate(haikus):
+        #get picsum image
+        response = requests.get('https://picsum.photos/740/525/?random') #dimensions of a postcard
+        img = Image.open(BytesIO(response.content))
+
+        #generate properties
+        font = ImageFont.truetype(random.choice(fonts), randint(30,40)) #choose typeface, font size from distributions
+        position = (randint(10,70),randint(100,300)) #choose x,y coordinates
+        color = getColor(img, position)
+
+        #write text and save
+        draw = ImageDraw.Draw(img)
+        draw.text(position, text, color,font=font)
+        img.save(os.path.join(directory,'haiku-{}.png'.format(index)))  
+
+def countSyllables(word):
+    word=re.sub(r"[^a-z]",'',word.lower())
+    if word in wordDict:
+        return len(list(y for y in wordDict[word][0] if y[-1] in '0123456789'))
+    return -1
+
+def getWords(sentence):
+    sentence=sentence.strip().replace('%',' percent').replace("'s "," ")
+    sentence=re.sub(r"(^|\s)-(\d+\.?\d*)",r" negative \2",sentence).strip() #deal with negative numbers (since we split on - later)
+    words=[word for word in re.split(r"\s+|-|–",sentence) if word]
+    i=0
+    while i<len(words):
+        word=words[i]
+        if word!='infinity' and word!='nan': #ignore cases that break num2words
+            try:
+                #if word is a number, convert to the spoken form so we can count syllables
+                conv=num2words(word).replace('point zero','').strip()
+                words.remove(word)
+                for newWord in re.split(r"\s+|-|–",conv):
+                    words.insert(i,newWord)
+                    i+=1
+            except TypeError:
+                pass #not a number
+        i+=1
+    return words
+
+def getColor(img, position):
+    #get avg lightness of pixels in the approximate location of the text
+    approxWidth=500
+    approxHeight=150
+    tot=0
+    for i in range(position[0], position[0]+approxWidth):
+        for j in range(position[1],position[1]+approxHeight):
+            tot+=sum(img.getpixel((i,j))) #add r+g+b
+    avg=tot/approxWidth/approxHeight/3
+
+    #take gray as far as possible from avg and flatten to black or white
+    if (avg+122)%255>122:
+        return (255,255,255)
+    else:
+        return (0,0,0)
+
+
+def main(verbose=False):
+    os.makedirs('images', exist_ok=True) #make images folder if doesn't already exist  
+    files = getFiles(filepath, verbose)  #get files
+    texts = extractTexts(files, verbose) #extract text from files
+    haiku = extractHaiku(texts, verbose) #extract haikus from text
+    displayHaiku(haiku, 'images', verbose) #display found haiku over images and save in /images
 
 if __name__=='__main__':
-    main()
+    main(True)
